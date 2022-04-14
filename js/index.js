a26c6cdde9a562f073e76af5005c911dcdbd09a3
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-04-14 02:40:49 -0300

    filtragem de mensagem privada implementado  Changes to be committed: 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+let nameGlobal ="";
 function initializeMessages(){
     logIn(prompt("Por favor insira seu lindo nome"));
 }
 function logIn(userName){
+
     const logInObject = {name: userName }
     const promisse=axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",logInObject);
     promisse.then(logInSucces(logInObject));
@@ -16,9 +18,11 @@ function logInError(error){
     }
 }
 function logInSucces(userNameObject){
-    requestMessages(userNameObject);
+    nameGlobal=userNameObject.name;
+    requestMessages(userNameObject );
     setInterval(requestMessages,3000,userNameObject)
 }
+//requestMessages também mantem o usuario conectado
 function requestMessages(name){
     const promisse= axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
     promisse.then(updateMessages);
@@ -29,28 +33,33 @@ function updateMessages(messageLog){
     const content= document.querySelector(".content");
     content.innerHTML = ""
     for(let messageNum=0;messageNum<messageLog.data.length;messageNum++){
-        const newMessage=document.createElement("div");
         const messageData = messageLog.data[messageNum];
         if(messageData.type === "message"){
+            const newMessage=document.createElement("div");
             newMessage.classList.add("message");
             newMessage.innerHTML=`<span class="timeText">(${messageData.time})</span>\u2002
             <span class="nameText">${messageData.from}</span>\u2002para\u2002
             <span class="nameText">${messageData.to}</span>:
-            \u0020${messageData.text}`
+            \u0020${messageData.text}`;
+            content.appendChild(newMessage);
         }
         else if(messageData.type ==="status"){
+            const newMessage=document.createElement("div");
             newMessage.classList.add("status");
             newMessage.innerHTML=`<span class="timeText">(${messageData.time})</span>\u2002
-            <span class="nameText">${messageData.from}</span>\u2002${messageData.text} `
+            <span class="nameText">${messageData.from}</span>\u2002${messageData.text} `;
+            content.appendChild(newMessage);
         }
-        else if(messageData.type === "private_message"){
+        else if((messageData.type === "private_message")&&((messageData.from===nameGlobal)||(messageData.to===nameGlobal))){
+            const newMessage=document.createElement("div");
             newMessage.classList.add("private_message");
             newMessage.innerHTML=`<span class="timeText">(${messageData.time})</span>\u2002
             <span class="nameText">${messageData.from}</span>\u2002reservadamente para\u2002
             <span class="nameText">${messageData.to}</span>:
-            \u0020${messageData.text} `
+            \u0020${messageData.text} `;
+            content.appendChild(newMessage);
         }
-        content.appendChild(newMessage);
+        
         content.lastChild.scrollIntoView();
     }
 
