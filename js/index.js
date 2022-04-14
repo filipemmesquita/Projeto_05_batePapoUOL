b818d18d6fde7b94811ad8af9073186710e8699d
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-04-14 01:45:30 -0300

    atualizar a cada 3 segundos implementado  Changes to be committed: 	modified:   css/style.css 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-let messageNum=0;
 function initializeMessages(){
     requestMessages();
+    setInterval(requestMessages,3000)
 }
 function requestMessages(){
     const promisse= axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
@@ -9,8 +9,8 @@ function requestMessages(){
 }
 function updateMessages(messageLog){
     const content= document.querySelector(".content");
-
-    for(;messageNum<messageLog.data.length;messageNum++){
+    content.innerHTML = ""
+    for(let messageNum=0;messageNum<messageLog.data.length;messageNum++){
         const newMessage=document.createElement("div");
         const messageData = messageLog.data[messageNum];
         console.log(messageData);
@@ -26,15 +26,17 @@ function updateMessages(messageLog){
             newMessage.innerHTML=`<span class="timeText">(${messageData.time})</span>\u2002
             <span class="nameText">${messageData.from}</span>\u2002${messageData.text} `
         }
-        content.appendChild(newMessage);
-        if(messageData.type === "private_message"){
+        else if(messageData.type === "private_message"){
             newMessage.classList.add("private_message");
             newMessage.innerHTML=`<span class="timeText">(${messageData.time})</span>\u2002
             <span class="nameText">${messageData.from}</span>\u2002reservadamente para\u2002
             <span class="nameText">${messageData.to}</span>:
             \u0020${messageData.text} `
         }
+        content.appendChild(newMessage);
+        content.lastChild.scrollIntoView();
     }
+
 }
 function catchError(erro) {
 	console.log("Erro " + erro.response.statuss);
