95307339ee794b16403486daf1955167ee15c4e2
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-04-14 22:55:10 -0300

    envio de mensagens implementado  Changes to be committed: 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let nameGlobal ="";
+let nameGlobal ={};
 function initializeMessages(){
     logIn(prompt("Por favor insira seu lindo nome"));
 }
@@ -18,20 +18,19 @@ function logInError(error){
     }
 }
 function logInSucces(userNameObject){
-    nameGlobal=userNameObject.name;
-    requestMessages(userNameObject );
-    setInterval(requestMessages,3000,userNameObject)
+    nameGlobal=userNameObject;
+    setInterval(requestMessages,3000);
 }
 //requestMessages também mantem o usuario conectado
-function requestMessages(name){
-    const promisse= axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+function requestMessages(){
+    const promisse= axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promisse.then(updateMessages);
     promisse.catch(catchError);
-    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", name).then(console.log,console.log) 
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nameGlobal).then(console.log,console.log); 
 }
 function updateMessages(messageLog){
     const content= document.querySelector(".content");
-    content.innerHTML = ""
+    content.innerHTML = "";
     for(let messageNum=0;messageNum<messageLog.data.length;messageNum++){
         const messageData = messageLog.data[messageNum];
         if(messageData.type === "message"){
@@ -40,30 +39,46 @@ function updateMessages(messageLog){
             newMessage.innerHTML=`<span class="timeText">(${messageData.time})</span>\u2002
             <span class="nameText">${messageData.from}</span>\u2002para\u2002
             <span class="nameText">${messageData.to}</span>:
-            \u0020${messageData.text}`;
+            \u2002${messageData.text}`;
             content.appendChild(newMessage);
         }
         else if(messageData.type ==="status"){
             const newMessage=document.createElement("div");
             newMessage.classList.add("status");
             newMessage.innerHTML=`<span class="timeText">(${messageData.time})</span>\u2002
-            <span class="nameText">${messageData.from}</span>\u2002${messageData.text} `;
+            <span class="nameText">${messageData.from}</span>\u2002${messageData.text}`;
             content.appendChild(newMessage);
         }
-        else if((messageData.type === "private_message")&&((messageData.from===nameGlobal)||(messageData.to===nameGlobal))){
+        else if((messageData.type === "private_message")&&((messageData.from===nameGlobal.name)||(messageData.to===nameGlobal.name))){
             const newMessage=document.createElement("div");
             newMessage.classList.add("private_message");
             newMessage.innerHTML=`<span class="timeText">(${messageData.time})</span>\u2002
             <span class="nameText">${messageData.from}</span>\u2002reservadamente para\u2002
             <span class="nameText">${messageData.to}</span>:
-            \u0020${messageData.text} `;
+            \u2002${messageData.text}`;
             content.appendChild(newMessage);
         }
-        
         content.lastChild.scrollIntoView();
     }
-
 }
 function catchError(erro) {
 	console.log("Erro " + erro.response.statuss);
+}
+function requestSendMessage(){
+    const messageField =document.querySelector(".bottom input");
+    if(messageField.value){
+        const messageObject ={
+            from: nameGlobal.name,
+            to: "Todos",
+            text: messageField.value,
+            type: "message"
+        }
+        const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", messageObject)
+        promisse.then(sendMessageSuccess)
+        promisse.catch(catchError)
+        messageField.value="";
+    }
+}
+function sendMessageSuccess(){
+    requestMessages();
 }
